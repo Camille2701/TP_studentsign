85401b4fe613be3e16198e9eda265ac4a7a7f4e3
Camille <veillardcamille01@gmail.com>
2023-11-23 23:21:43 +0100

    Configuration de l'app react avec typescript

---
+++ src/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage: React.FC = () => {
+    return (
+        <div>
+            <h1>Bienvenue sur la page d'accueil</h1>
+            <p>Ceci est la page d'accueil de mon application.</p>
+        </div>
+    );
+}
+
+export default HomePage;
